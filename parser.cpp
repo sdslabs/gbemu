6bd01678b7c281839c3ae282d5ce28fb08761261
Parth-Nebula <parthmittal.nebula@gmail.com>
2022-08-05 01:49:23 +0530

    create function checkNintendoLogo

--- parser.cpp
+++ parser.cpp
@@ -30,3 +30,20 @@ bool makeGameHeader ( GameHeader *gameHeader )
 	return 1 ;
 
 }
+
+bool checkNintendoLogo ( GameHeader *gameHeader )
+{
+
+	uint8_t nintendo_logo[48] = { 206,237,102,102,204,13,0,11,3,115,0,131,0,12,0,13,0,8,17,31,136,137,0,14,220,204,110,230,221,221,217,153,187,187,103,99,110,14,236,204,221,220,153,159,187,185,51,62 } ;
+
+	for ( int i = 0 ; i < 48 ; i ++ )
+	{
+		if ( nintendo_logo[i] != (gameHeader->nintendo_logo)[i] )
+		{
+			return 0 ;
+		}
+	}
+	
+	return 1 ;
+
+}
