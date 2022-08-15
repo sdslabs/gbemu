e6d8063f8818fc72ea398203f3a7b321778e10c2
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2022-08-15 22:12:22 +0530

    Create ReadStream

--- gbemu/gbemu/common/stream.h
+++ gbemu/gbemu/common/stream.h
@@ -4,14 +4,25 @@
 
 namespace Common {
 	class Stream {
-        virtual ~Stream() {}
+        public:
+            virtual ~Stream() {}
 
-        // Below functions throw error and reset the error flag
-        // Similar to ferror() and clearerr()
+            // Below functions throw error and reset the error flag
+            // Similar to ferror() and clearerr()
 
-        virtual bool err() const { return false; }
-        virtual void clearError() {}
+            virtual bool err() const { return false; }
+            virtual void clearError() {}
 	};
 
+    class ReadStream : virtual public Stream {
+        public:
+	        ReadStream() {}
+
+            // End of stream interface
+            virtual bool eos() const = 0;
+
+            // Read interface
+            virtual uint32 read(void *dataPtr, uint32 dataSize) = 0;
+    };
 
 }
