29bf0a1ecaa82c9fff1e53fb7d4ce944f335da0f
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2022-08-15 22:39:58 +0530

    complete endianness.h

--- gbemu/gbemu/common/endianness.h
+++ gbemu/gbemu/common/endianness.h
@@ -30,5 +30,70 @@
 
 
 // We assume the machine to be Little Endian by default
+// As this is running on Intel, AMD and Apple Silicon chips
+// Will add compatibility for Big Endian machine later
+
+inline uint16 READ_UINT16(const void *ptr) {
+	const uint8 *b = (const uint8 *)ptr;
+	return (b[1] << 8) | b[0];
+}
+
+inline uint32 READ_UINT32(const void *ptr) {
+	const uint8 *b = (const uint8 *)ptr;
+	return (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | (b[0]);
+}
+
+inline uint64 READ_UINT64(const void *ptr) {
+	const uint8 *b = (const uint8 *)ptr;
+	return ((uint64)b[7] << 56) | ((uint64)b[6] << 48) | ((uint64)b[5] << 40) | ((uint64)b[4] << 32) | ((uint64)b[3] << 24) | ((uint64)b[2] << 16) | ((uint64)b[1] << 8) | ((uint64)b[0]);
+}
+
+inline void WRITE_UINT16(void *ptr, uint16 value) {
+	uint8 *b = (uint8 *)ptr;
+	b[0] = (uint8)(value >> 0);
+	b[1] = (uint8)(value >> 8);
+}
+
+inline void WRITE_UINT32(void *ptr, uint32 value) {
+	uint8 *b = (uint8 *)ptr;
+	b[0] = (uint8)(value >>  0);
+	b[1] = (uint8)(value >>  8);
+	b[2] = (uint8)(value >> 16);
+	b[3] = (uint8)(value >> 24);
+}
+
+inline void WRITE_UINT64(void *ptr, uint64 value) {
+	uint8 *b = (uint8 *)ptr;
+	b[0] = (uint8)(value >>  0);
+	b[1] = (uint8)(value >>  8);
+	b[2] = (uint8)(value >> 16);
+	b[3] = (uint8)(value >> 24);
+	b[4] = (uint8)(value >> 32);
+	b[5] = (uint8)(value >> 40);
+	b[6] = (uint8)(value >> 48);
+	b[7] = (uint8)(value >> 56);
+}
+
+// Used in Common::Stream
+#define READ_LE_UINT16(a) READ_UINT16(a)
+#define READ_LE_UINT32(a) READ_UINT32(a)
+#define READ_LE_UINT64(a) READ_UINT64(a)
+#define WRITE_LE_UINT16(a, v) WRITE_UINT16(a, v)
+#define WRITE_LE_UINT32(a, v) WRITE_UINT32(a, v)
+#define WRITE_LE_UINT64(a, v) WRITE_UINT64(a, v)
+#define FROM_LE_16(a) ((uint16)(a))
+#define FROM_LE_32(a) ((uint32)(a))
+#define FROM_LE_64(a) ((uint64)(a))
+#define FROM_BE_16(a) SWAP_BYTES_16(a)
+#define FROM_BE_32(a) SWAP_BYTES_32(a)
+#define FROM_BE_64(a) SWAP_BYTES_64(a)
+#define TO_LE_16(a) ((uint16)(a))
+#define TO_LE_32(a) ((uint32)(a))
+#define TO_LE_64(a) ((uint64)(a))
+#define TO_BE_16(a) SWAP_BYTES_16(a)
+#define TO_BE_32(a) SWAP_BYTES_32(a)
+#define TO_BE_64(a) SWAP_BYTES_64(a)
+
+// Might come handy later
 #define LITTLE_ENDIAN true
 #define BIG_ENDIAN false
