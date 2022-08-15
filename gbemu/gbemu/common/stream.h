4f2d0f3597eb4db0a94282f7d6748b524cb31339
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2022-08-15 22:05:21 +0530

    Create Stream

--- gbemu/gbemu/common/stream.h
+++ gbemu/gbemu/common/stream.h
@@ -1,3 +1,17 @@
 //Taken from ScummVM's Common::Stream
 
 #include "endianness.h"
+
+namespace Common {
+	class Stream {
+        virtual ~Stream() {}
+
+        // Below functions throw error and reset the error flag
+        // Similar to ferror() and clearerr()
+
+        virtual bool err() const { return false; }
+        virtual void clearError() {}
+	};
+
+
+}
