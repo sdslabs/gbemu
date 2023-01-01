a036eca6ddd270ff724b856dbffcd414ce4fb5c2
Aviii06 <architgosain0601@gmail.com>
2023-01-02 03:19:22 +0530

    Fix formatting, update CMake and add ReadME

--- src/gameBoy.cpp
+++ src/gameBoy.cpp
@@ -3,25 +3,25 @@
 #include "gameBoy.h"
 
 GBE::GBE()
-{   
-    // Initialize the CPU
-    gbe_cpu = new CPU();
+{
+	// Initialize the CPU
+	gbe_cpu = new CPU();
 }
 
 void GBE::update()
 {
-    // Update function of the GBE
-    // Will be called every frame
-    // GB has 59.73 frames per second
+	// Update function of the GBE
+	// Will be called every frame
+	// GB has 59.73 frames per second
 
-    int cycles = 0;
-    while (cycles < gbe_cpu->clockSpeedPerFrame)
-    {
-        // Execute the next instruction
-        // cycles += gbe_cpu->executeNextInstruction();
-        // updateTimers()
-        // updateGraphics()
-        // Do Interrupts()
-    }
-    // renderGraphics()
+	int cycles = 0;
+	while (cycles < gbe_cpu->clockSpeedPerFrame)
+	{
+		// Execute the next instruction
+		// cycles += gbe_cpu->executeNextInstruction();
+		// updateTimers()
+		// updateGraphics()
+		// Do Interrupts()
+	}
+	// renderGraphics()
 }
