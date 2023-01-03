466843f02940a9af15e1a9a7991b50eefd39ded7
Aviii06 <architgosain0601@gmail.com>
2023-01-04 05:23:37 +0530

    Wrote few opcodes forgot to push

--- src/cpu.h
+++ src/cpu.h
@@ -38,6 +38,9 @@ private:
 	// Program Counter
 	Register reg_PC;
 
+	// Low PowerMode Bool
+	bool isLowPower;
+
 	// Flags
 	// Pulled from https://gbdev.io/pandocs/CPU_Registers_and_Flags.html
 	// Naming convention is: FLAG_<name>_<bit>
