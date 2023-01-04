d39ac0647f22f333299fdfe61dce38a9bfb4dde5
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2023-01-04 05:43:43 +0530

    Write Test ROM till INC B

--- src/gameBoy.cpp
+++ src/gameBoy.cpp
@@ -13,33 +13,61 @@ GBE::GBE()
 	// Unify the CPU and MemoryMap
 	gbe_cpu->setMemory(gbe_mMap);
 
+	// Current State: AF = 0x01B0, BC = 0x0013, DE = 0x00D8, HL = 0x014D, SP = 0xFFFE, PC = 0x0100
+
+	// NOP
+	// Does nothing
+	// Final State: No change
 	gbe_mMap->debugWriteMemory(0x0100, 0x00);
 
-	// Dec B
-	// Test
-	// zero flag should be set
+	// LD BC, u16
+	// Loads a 16 bit immediate into the register BC
+	// Final State: BC = 0XE001
 	gbe_mMap->debugWriteMemory(0x0101, 0x01);
-	gbe_mMap->debugWriteMemory(0x0102, 0x01);
-	gbe_mMap->debugWriteMemory(0x0103, 0x00);
-	gbe_mMap->debugWriteMemory(0x0104, 0x05);
-	// half carry flag should be set
-	gbe_mMap->debugWriteMemory(0x0105, 0x01);
-	gbe_mMap->debugWriteMemory(0x0106, 0x10);
-	gbe_mMap->debugWriteMemory(0x0107, 0x00);
-	gbe_mMap->debugWriteMemory(0x0108, 0x05);
+	gbe_mMap->debugWriteMemory(0x0102, 0xE0);
+	gbe_mMap->debugWriteMemory(0x0103, 0x01);
 
+	// LD (BC), A
+	// Loads the value of the accumulator into the memory address pointed to by BC
+	// Final State: Value 0x01 at address 0xE001 (2nd byte of Work RAM)
+	gbe_mMap->debugWriteMemory(0x0104, 0x02);
 
+	// INC BC
+	// Increments the value of BC by 1
+	// Final State: BC = 0xE002
+	gbe_mMap->debugWriteMemory(0x0105, 0x03);
 
-//	//RLCA
-//	gbe_mMap->debugWriteMemory(0x0103, 0x07);
-//
-//	// LD (BC), A
-//	gbe_mMap->debugWriteMemory(0x0104, 0x02);
-//
-//	// LD (u16), A
-//	gbe_mMap->debugWriteMemory(0x0105, 0xEA);
-//	gbe_mMap->debugWriteMemory(0x0106, 0x69);
-//	gbe_mMap->debugWriteMemory(0x0107, 0x69);
+	// INC B
+	// Increments the value of B by 1
+	// Final State: BC = 0xE102, Flag_N = 0, Flag_H = 0, Flag_Z = 0
+	gbe_mMap->debugWriteMemory(0x0106, 0x04);
+
+	// Set BC = 0XFFFF to test INC B for Z flag
+	gbe_cpu->set_reg_BC(0xFFFF);
+
+	// INC B
+	// Increments the value of B by 1
+	// Final State: BC = 0x00FF, Flag_N = 0, Flag_H = 0, Flag_Z = 1
+	gbe_mMap->debugWriteMemory(0x0107, 0x04);
+
+	// Set BC = 0X0FFF to test INC B for H flag
+	gbe_cpu->set_reg_BC(0x0FFF);
+
+	// INC B
+	// Increments the value of B by 1
+	// Final State: BC = 0x10FF, Flag_N = 0, Flag_H = 1, Flag_Z = 0
+	gbe_mMap->debugWriteMemory(0x0108, 0x04);
+
+	// //RLCA
+	// gbe_mMap->debugWriteMemory(0x0103, 0x07);
+
+	// // LD (BC), A
+	// gbe_mMap->debugWriteMemory(0x0104, 0x02);
+
+	// // LD (u16), A
+	// gbe_mMap->debugWriteMemory(0x0105, 0xEA);
+	// gbe_mMap->debugWriteMemory(0x0106, 0x69);
+	// gbe_mMap->debugWriteMemory(0x0107, 0x69);
 
 	// Seg fault to end using UNKOWN
 	gbe_mMap->debugWriteMemory(0x0109, 0xEB);
